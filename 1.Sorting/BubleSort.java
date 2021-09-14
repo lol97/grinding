82598d52a11a7b48aed8b3e15e34148fb8e46a17
Warteg Dust <blackmanga80@gmail.com>
2021-09-14 22:04:31 +0700

    buble sort array

---
+++ 1.Sorting/BubleSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class BubleSort {
+	public BubleSort() {
+
+	}
+
+	public int[] doBubleSort(int[] unsortedData) {
+		int temp;
+		int length = unsortedData.length;
+		for (int i=0; i < length - 1; i++) {
+			for (int j=0; j < length-i-1; j++ ) {
+				if (unsortedData[j] > unsortedData[j+1]) {
+					temp = unsortedData[j];
+					unsortedData[j] = unsortedData[j+1];
+					unsortedData[j+1] = temp;
+				}
+			}
+		}
+		return unsortedData;
+	}
+
+	public static void main(String[] args) {
+
+		int data[] = {1, 4, 2, 5, 8};
+		BubleSort bs = new BubleSort();
+		
+		System.out.println(Arrays.toString(bs.doBubleSort(data)));
+	}
+}
